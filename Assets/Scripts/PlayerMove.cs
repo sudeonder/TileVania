27f89d118e66b56e0be10ee575ece4f395c54eb4
Sude Önder <sudeonder999@gmail.com>
2024-04-25 20:40:34 +0300

    control running animation

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -8,15 +8,18 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] float moveSpeed;
     Vector2 moveInput;
     Rigidbody2D rb;
+    Animator animator;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
@@ -27,5 +30,15 @@ public class PlayerMove : MonoBehaviour
     void Run()
     {
         rb.velocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        // set animation state to running
+        animator.SetBool("isRunning", moveInput.x != 0);
+
+    }
+
+    void FlipSprite()
+    {
+        // only flip if there is movement
+        if (moveInput.x == 0) return;
+        transform.localScale = new Vector3(Mathf.Sign(moveInput.x), 1, 1);
     }
 }
